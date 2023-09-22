1fb056f1fe8670ab1f28c185b10345d9872a1db6
Trevor <jtantley@gmail.com>
2023-09-22 16:08:32 -0500

    Update openai_module.py

--- core/clericus_modules/openai_module.py
+++ core/clericus_modules/openai_module.py
@@ -1,11 +1,11 @@
 # Clericus Module: OpenAI Module
 # Version: v0.0.3-dev
 # Path: `\core\clericus_modules\openai_module.py`
-# Updated: 09-21-2023
+# Updated: 09-22-2023
 
 import openai
 import os
-from log_config import handle_openai_error, handle_generic_error, clericus_logger
+from ..log_config import handle_openai_error, handle_generic_error, clericus_logger
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 openai.api_key = OPENAI_API_KEY
