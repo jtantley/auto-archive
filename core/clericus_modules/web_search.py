a07a57ef8463f47d8db5616456647bd1277937d5
Trevor <jtantley@gmail.com>
2023-09-24 00:13:49 -0500

    Update web_search.py

--- core/clericus_modules/web_search.py
+++ core/clericus_modules/web_search.py
@@ -1,18 +1,19 @@
 # Clericus Module: Web Search Function
 # Version: v0.0.3-dev
 # Path: `\core\clericus_modules\web_search.py`
-# Updated: 09-22-2023
+# Updated: 09-23-2023
 
 import requests
-import os
-from ..log_config import handle_generic_error, clericus_logger
+from ..logger import handle_generic_error, clericus_logger
+from typing import Union
+from core.config import SERPAPI_API_KEY
 
-SERP_API_KEY = os.getenv('SERP_API_KEY')
+# Serpstack API Search Function
 
 
-def perform_web_search(query):
+def perform_web_search(query: str) -> Union[dict, str]:
     try:
-        search_url = f'https://api.serpstack.com/search?access_key={SERP_API_KEY}&query={query}'
+        search_url = f'https://api.serpstack.com/search?access_key={SERPAPI_API_KEY}&query={query}'
         response = requests.get(search_url)
         response.raise_for_status()
         search_results = response.json()
