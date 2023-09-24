3060834a47e55e50e02ca564908371fd681bc2de
Trevor <jtantley@gmail.com>
2023-09-24 14:25:33 -0500

    Update clericus.py

--- core/clericus.py
+++ core/clericus.py
@@ -1,7 +1,7 @@
 # Auto-Archive: Clericus Module
 # Version: v0.0.3-dev
 # Path: `\core\clericus.py`
-# Updated: 09-23-2023
+# Updated: 09-24-2023
 
 # ⚠️ ACTIVE DEVELOPMENT ⚠️ #
 
@@ -30,3 +30,33 @@ def generate_response(user_input: str, session_id: str) -> Union[str, dict]:
     except Exception as e:
         error_response = handle_generic_error(e, clericus_logger)
         return json.dumps(error_response)  # Convert object to JSON string
+# Function to format the response text into more readable chunks like paragraphs or lists
+
+
+def format_response_text(response_text: str) -> str:
+    # Keywords or phrases that could indicate a list or new paragraph
+    list_indicators = ["- ", "* ", "1. ", "2. ", "3. "]
+    paragraph_indicators = ["\n\n"]
+
+    # Initialize variables
+    formatted_text = ""
+    current_line = ""
+
+    # Tokenize the text by spaces
+    tokens = response_text.split(" ")
+
+    for token in tokens:
+        current_line += token + " "
+
+        # Check for list or new paragraph indicators
+        if any(indicator in current_line for indicator in list_indicators):
+            formatted_text += "\n- " + current_line.strip()
+            current_line = ""
+        elif any(indicator in current_line for indicator in paragraph_indicators):
+            formatted_text += "\n\n" + current_line.strip()
+            current_line = ""
+
+    # Append any remaining text
+    formatted_text += current_line.strip()
+
+    return formatted_text
