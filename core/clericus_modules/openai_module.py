dc6f4dc4eb3b5c740671fe47f827ae7f7e7d288a
Trevor <jtantley@gmail.com>
2023-09-24 00:13:16 -0500

    Update openai_module.py

--- core/clericus_modules/openai_module.py
+++ core/clericus_modules/openai_module.py
@@ -1,33 +1,40 @@
+
 # Clericus Module: OpenAI Module
 # Version: v0.0.3-dev
 # Path: `\core\clericus_modules\openai_module.py`
-# Updated: 09-22-2023
+# Updated: 09-23-2023
 
-import openai
 import os
-from ..log_config import handle_openai_error, handle_generic_error, clericus_logger
+import openai
+from core.logger import handle_generic_error, clericus_logger
+from core.config import OPENAI_API_KEY
+from typing import Union
 
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-openai.api_key = OPENAI_API_KEY
 
-
-def generate_openai_response(prompt, session_id):
+def generate_openai_response(prompt: str, session_id: str) -> Union[str, dict]:
+    """
+    Generate OpenAI API response based on prompt & session ID
+    """
     try:
         model_engine = os.getenv('MODEL_ENGINE', 'gpt-3.5-turbo-16k')
-        response = openai.Completion.create(
-            engine=model_engine,
-            prompt=prompt,
-            max_tokens=2000,
-            n=1,
-            stop=None,
+        response = openai.ChatCompletion.create(
+            model=model_engine,
+            messages=[
+                {
+                    "role": "system",
+                    "content": "Your name is Clericus. You are a helpful assistant."
+                },
+                {
+                    "role": "user", "content": prompt
+                }
+            ],
             temperature=1,
+            max_tokens=3000,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=0
         )
-        assistant_reply = response.choices[0].text.strip()
+        assistant_reply = response['choices'][0]['message']['content'].strip()
         return assistant_reply
-    except openai.Error as e:
-        return handle_openai_error(e, clericus_logger)
-    except Exception as e:
+    except Exception as e:  # Catching generic exception
         return handle_generic_error(e, clericus_logger)
